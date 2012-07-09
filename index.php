5f55838e511e274bc29fb6677b5314e0f7ddf7e3
phpms <phpms@cyclops.dreamhost.com>
2012-07-09 11:49:59 -0700

    Versão inicial

---
+++ index.php
@@ -0,0 +1,298 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<meta name="description" content="PHPMS Conf 2012" />
+		<meta name="keywords" content="HTML,CSS,Web,PHP,PHPMS,Conf" />
+		<meta charset="utf-8">
+
+		<title>PHPMS Conf 2012</title>
+		<link href='http://fonts.googleapis.com/css?family=Pontano+Sans' rel='stylesheet' type='text/css' />
+
+		<link href="css/screen.css" media="all" rel="stylesheet" type="text/css" />
+		<link href="css/960_12_col.css" media="all" rel="stylesheet" type="text/css" />
+
+		<script type="text/javascript" src="http://use.typekit.com/hdt5icj.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+		<!--[if lte IE 8]>
+		<script type="text/javascript">
+		var htmlshim='abbr,article,aside,audio,canvas,details,figcaption,figure,footer,header,mark,meter,nav,output,progress,section,summary,time,video'.split(',');
+		var htmlshimtotal=htmlshim.length;
+		for(var i=0;i<htmlshimtotal;i++) document.createElement(htmlshim[i]);
+		</script>
+		<![endif]-->
+	</head>
+	<body>
+		<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+		<div class="container_12">
+			<div class="social">
+				<a href="https://twitter.com/share" class="twitter-share-button" data-text="Não deixa para a última hora, inscreva-se agora mesmo no PHPMS Conf 2012" data-via="phpms" data-lang="pt" data-hashtags="phpmsconf">Tweetar</a>
+				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+				<div class="fb-like" data-href="https://www.facebook.com/DesenvolvedoresMS" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div>
+			</div>
+			<header class="grid_10 push_1">
+				<h1 class="rp">PHPMS Conf 2012</h1>
+				<h2 class="rp">
+				17 e 18 de Agosto
+				Campo Grande
+				Mato Grosso do Sul
+				</h2>
+			</header>
+
+			<section class="grid_6" id="evento">
+				<h2 class="rp grid_3 push_1">O Evento</h2>
+
+				<div class="clear"></div>
+
+				<p class="grid_6 push_1">
+				A PHPMS Conf é um evento voltado a profissionais e entusiastas da linguagem PHP, é a sequência de 3 Workshops realizados pelo grupo em seus primeiros anos.
+				Este ano o evento vem remodelado e renovado, com muitas novidades e a presença de profissionais reconhecimentos nacionalmente na comunidade PHP através de suas
+				contribuições e participações em eventos de renome. Uma novidade nesta edição é a parceria inédita com o Startup MS, fomentando o empreendedorismos entre os
+				desenvolvedores de software e a participação deste na tradicional mesa-redonda.
+				Não perca essa oportunidade única de participar de um dos maiores eventos voltados ao Desenvolvimento Web do Mato Grosso do Sul e região.
+				</p>
+			</section>
+
+			<section id="local" class="grid_11">
+				<h2 class="rp">Local</h2>
+
+				<div class="clear"> </div>
+
+				<p> Senac/MS - Rua 26 de Agosto - Centro </p>
+				<p> Próximo ao Horto Florestal </p>
+				<p> Campo Grande - MS </p>
+
+				<p><a target="_blank" href="http://goo.gl/Wfu8K" id="botao_google" class="rp"></a></p>
+			</section>
+
+			<section id="palestras" class="grid_11 push_1">
+				<div class="grid_11 alpha">
+					<h2 class="rp">Palestras</h2>
+				</div>
+
+				<div class="grid_4">
+					<h3 class="rp" id="dia17">Dia 17</h3>
+
+					<div class="grid_4 alpha palestras_item">
+						<div class="grid_1 alpha circuloamarelo">
+							<h5 class="grid_1 alpha">?</h5>
+						</div>
+
+						<div class="grid_3 omega">
+							<h4>Frameworks de desenvolvimento em PHP</h4>
+							<p>Apresentação dos principais frameworks existentes em PHP, suas aplicações, vantagens e desvantagens.</p>
+						</div>
+
+						<!--						<div class="grid_1 alpha circuloazulp">
+							<h5 class="grid_1 alpha">8h30</h5>
+						</div>
+
+						<div class="grid_3 omega">
+							<h4>Fulano da Silva</h4>
+													<p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat.</p>
+						</div>
+
+						<div class="grid_1 alpha circulolaranja">
+							<h5 class="grid_1 alpha">8h30</h5>
+						</div>
+
+						<div class="grid_3 omega">
+							<h4>Fulano da Silva</h4>
+													<p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat.</p>
+						</div>
+
+						<div class="grid_1 alpha circuloazulb">
+							<h5 class="grid_1 alpha">8h30</h5>
+						</div>
+
+						<div class="grid_3 omega">
+							<h4>Fulano da Silva</h4>
+													<p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat.</p>
+						</div>-->
+					</div>
+				</div>
+
+				<div class="grid_4 push_2">
+					<h3 class="rp" id="dia18">Dia 18</h3>
+
+					<div class="grid_4 alpha palestras_item">
+						<div class="grid_1 alpha circuloamarelo">
+							<h5 class="grid_1 alpha">?</h5>
+						</div>
+
+						<div class="grid_3 omega">
+							<h4>PHP like a Super Hero</h4>
+							<p>Apresentação de tecnologias e teorias que um programador iniciante em PHP deve aprender para tornar-se um profissional de alto nível.</p>
+						</div>
+
+						<div class="grid_1 alpha circuloazulp">
+							<h5 class="grid_1 alpha">?</h5>
+						</div>
+
+						<div class="grid_3 omega">
+							<h4>REST: Faça o Serviço Direito</h4>
+							<p>Uma explicação sobre REST e Hipermídia, sua evolução histórica e exemplos naturais de seu uso com foco em resolução de problemas.</p>
+						</div>
+
+						<!--						<div class="grid_1 alpha circulolaranja">
+							<h5 class="grid_1 alpha">8h30</h5>
+						</div>
+
+						<div class="grid_3 omega">
+							<h4>Fulano da Silva</h4>
+													<p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat.</p>
+						</div>
+
+						<div class="grid_1 alpha circuloazulb">
+							<h5 class="grid_1 alpha">8h30</h5>
+						</div>
+
+						<div class="grid_3 omega">
+							<h4>Fulano da Silva</h4>
+													<p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat.</p>
+						</div>-->
+					</div>
+				</div>
+
+			</section>
+
+			<section id="palestrantes" class="grid_11 push_1">
+
+				<h2 class="rp">Palestrantes</h2>
+
+				<div class="grid_11 alpha"></div>
+
+				<div class="grid_4">
+					<div class="grid_4 alpha palestras_item">
+						<div class="grid_2 alpha">
+							<img src="images/speakers/gaigalas.png">
+						</div>
+
+						<div class="grid_2 omega palestrante">
+							<h4>Alexandre Gaigalas</h4>
+							<p>Desenvolvedor no Yahoo! Brasil, entusiasta de arquiteturas RESTful e Web Semântica.</p>
+						</div>
+
+						<div class="grid_2 alpha">
+							<img src="images/avatar.png">
+						</div>
+
+						<div class="grid_2 omega palestrante">
+							<h4>Você!</h4>
+							<p>A <a href="http://phpms.org/conf/2012/chamada-de-trabalhos/"><strong>chamada de trabalhos</strong></a> ainda está aberta, corra e submeta sua atividade ao PHPMS Conf 2012</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="grid_4 push_2">
+					<div class="grid_4 alpha palestras_item">
+						<div class="grid_2 alpha">
+							<img src="images/speakers/minetto.png">
+						</div>
+
+						<div class="grid_2 omega palestrante">
+							<h4>Elton Luís Minetto</h4>
+							<p>Desenvolvedor desde 1998, trabalhando com PHP, Python, Linux e bancos de dados. Sócio da Coderockr, escritor e professor.</p>
+						</div>
+
+						<!--						<div class="grid_2 alpha">
+													<img src="images/avatar.png">
+						</div>
+
+						<div class="grid_2 omega palestrante">
+							<h4>Fulano da Silva</h4>
+													<p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat.</p>
+						</div>-->
+					</div>
+				</div>
+
+			</section>
+
+			<section id="patrocinio" class="grid_11 push_1">
+
+				<h2 class="rp">Patrocínio e Apoio</h2>
+
+				<div class="grid_10 alpha">
+
+					<h5>Diamante</h5>
+
+					<div class="ret_diamante">
+						<a href="http://ms.senac.br/" title="SENAC MS"><img src="images/patrocinio/b_senac.png" alt="SENAC MS" /></a>
+					</div>
+
+					<h5>Ouro</h5>
+
+					<div class="ret_ouro">
+						<a href="http://catwork.com.br/" title="Catwork Soluções em Software"><img src="images/patrocinio/b_catwork.png" alt="Catwork" /></a>
+
+						<a href="http://jera.com.br/" title="Jera Desenvolvimento Web, Android e iOS"><img src="images/patrocinio/b_jera.png" alt="Jera" /></a>
+
+						<a href="http://radig.com.br/" title="Radig Soluções em TI"><img src="images/patrocinio/b_radig.png" alt="Radig Soluções em TI" /></a>
+					</div>
+
+					<h5>Prata</h5>
+
+					<div class="ret_prata">
+						<a href="http://metodoi.com.br/" title="Método Informática"><img src="images/patrocinio/b_metodo.png" alt="Método Informática" /></a>
+					</div>
+
+					<h5>Bronze</h5>
+
+					<div class="ret_bronze">
+						<a href="http://acicg.com.br/" title="Associação Comercial e Industrial de Campo Grande"><img src="images/patrocinio/b_acicg.png" alt="ACICG" /></a>
+						<a href="http://dothcom.com.br/" title="dothCom Consultoria Digital"><img src="images/patrocinio/b_dothcom.png" alt="dothCom" /></a>
+						<a href="http://gestaoativa.com.br/" title="Gestão Ativa Soluções Web"><img src="images/patrocinio/b_gestaoativa.png" alt="Gestão Ativa" /></a>
+						<a href="http://spidersofthouse.com/" title="Spider SoftHouse"><img src="images/patrocinio/b_spider.png" alt="Spider SoftHouse" /></a>
+						<a href="http://tecsinapse.com.br/" title="TecSinapse"><img src="images/patrocinio/b_tecsinapse.png" alt="TecSinapse" /></a>
+					</div>
+
+					<h5>Apoio</h5>
+
+					<div class="ret_apoio">
+						<a href="http://fmb.org.br/" title="Fundação Manoel de Barros"><img src="images/patrocinio/b_fundacao.png" alt="Fundação Manoel de Barros" /></a>
+					</div>
+
+				</div>
+
+			</section>
+
+			<section>
+				<div id="inscricoes_1" class="grid_6 push_3">
+					<div class="grid_3 push_2 tituloinscricoes alpha"> </div>
+					<div id="banner_inscricoes" class="grid_6"> </div>
+					<div> <a href="http://comitiva.phpms.org/divulgacao/phpms-conf-2012" id="botao_inscrever" class="grid_3 push_3 omega"> </a> </div>
+				</div>
+
+				<div class="clear"></div>
+			</section>
+
+			<div class="clear"> </div>
+
+			<section>
+
+				<div class="grid_6 push_3">
+					<div class="grid_3 push_2 contato alpha"></div>
+				</div>
+
+				<div class="clear"></div>
+
+				<div class="contatotexto">
+					<div class="clear"></div>
+					<a target="_blank" href="http://phpms.org/" title="Site do PHPMS"><img src="images/icones/site.png" alt="Site do PHPMS" /></a>
+					<a href="mailto:admin@phpms.org" title="Envie-nos um e-mail"><img src="images/icones/email.png" alt="Envie-nos um e-mail" /></a>
+					<a target="_blank" href="https://www.facebook.com/DesenvolvedoresMS" title="Curta nossa página no Facebook"><img src="images/icones/facebook.png" alt="Curta nossa página no Facebook" /></a>
+					<a target="_blank" href="http://twitter.com/#!/phpms" title="Nos siga no Twitter"><img src="images/icones/twitter.png" alt="Nos siga no Twitter" /></a>
+					<div class="clear"></div>
+					<div class="logophp"> </div>
+				</div>
+			</section>
+		</div>
+	</body>
+</html>
